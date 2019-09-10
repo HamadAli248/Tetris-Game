f0c130ba8e15ffbc58b3d9948a48920c87830cd8
Hamad Ali <hammadali248@gmail.com>
2019-09-10 09:36:22 +0100

    Updates

---
+++ precache-manifest.93e11278d6827abe15bb1f128981596b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57eea419683c2cd121c18d37d3a5c02",
+    "url": "/HamadAli248/Tetris-Game.git/index.html"
+  },
+  {
+    "revision": "4fc2196e7fbc7722304f",
+    "url": "/HamadAli248/Tetris-Game.git/static/css/main.0335de7c.chunk.css"
+  },
+  {
+    "revision": "5768386995b6d2911272",
+    "url": "/HamadAli248/Tetris-Game.git/static/js/2.c11591bc.chunk.js"
+  },
+  {
+    "revision": "4fc2196e7fbc7722304f",
+    "url": "/HamadAli248/Tetris-Game.git/static/js/main.6399b60e.chunk.js"
+  },
+  {
+    "revision": "e7126141421683e6a9cc",
+    "url": "/HamadAli248/Tetris-Game.git/static/js/runtime~main.49b12870.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/HamadAli248/Tetris-Game.git/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/HamadAli248/Tetris-Game.git/static/media/bg.2810fe6b.png"
+  }
+]);
